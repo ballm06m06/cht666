bf9285830da57ae765fa767585d9643350607fea
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-11 23:42:21 +0800

    init

---
+++ app.py
@@ -0,0 +1,57 @@
+# encoding: utf-8
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+handler = WebhookHandler('2c0174c6d045c85e0d07e97589a28f2a') 
+line_bot_api = LineBotApi('CM4iLIdM/vkrSSvcr+UP+vYIZwoG5sLE7aZxL+XzcP9Gsz4G8uIyFKi87uUKbEvBDBQ99Zr64Gu5VOd5fky6XQBTwSz38t25AK9v6JKv57z/sl3OuFbo4X+8f4pEaBTYcdRi8nsVXt6VH5/DqQGejgdB04t89/1O/w1cDnyilFU=') 
+
+
+@app.route('/')
+def index():
+    return "<p>Hello cht!</p>"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+# ================= 機器人區塊 Start =================
+@handler.add(MessageEvent, message=TextMessage)  # default
+def handle_text_message(event):                  # default
+    msg = event.message.text #message from user
+
+    # 針對使用者各種訊息的回覆 Start =========
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=msg))
+
+    # 針對使用者各種訊息的回覆 End =========
+
+# ================= 機器人區塊 End =================
+
+import os
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=os.environ['PORT'])
