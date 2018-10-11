980bc6e7c9df4cb83d32f706d7000ef768a8a078
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-11 23:59:54 +0800

    package

---
+++ cht_package/config.py
@@ -0,0 +1,3 @@
+# line bot key
+line_channel_access_token = 'CM4iLIdM/vkrSSvcr+UP+vYIZwoG5sLE7aZxL+XzcP9Gsz4G8uIyFKi87uUKbEvBDBQ99Zr64Gu5VOd5fky6XQBTwSz38t25AK9v6JKv57z/sl3OuFbo4X+8f4pEaBTYcdRi8nsVXt6VH5/DqQGejgdB04t89/1O/w1cDnyilFU='
+line_channel_secret = '2c0174c6d045c85e0d07e97589a28f2a'
