3dbddee3e8d8ca4b2fa867c5f5e27f0c1fa2a944
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-12 10:50:08 +0800

    olami

--- text_input/olami.py
+++ text_input/olami.py
@@ -5,7 +5,7 @@ from cht_package.config import OLAMI_APP_KEY, OLAMI_APP_SECRET, OLAMI_URL
 def OLAMI_textInput(input):
     nluApi = NluAPISample()
     nluApi.setLocalization(OLAMI_URL)
-    nluApi.setAuthorization('c97b4c08105144ee874c7329aa9e076a', '17256f7d09e34f8983cd20f60c5d9ae8')     
+    nluApi.setAuthorization(OLAMI_APP_KEY, OLAMI_APP_SECRET)     
     
     print("\n---------- Test NLU API, api=seg ----------\n");
     print("\nResult:\n\n", nluApi.getRecognitionResult(nluApi.API_NAME_SEG, input))
