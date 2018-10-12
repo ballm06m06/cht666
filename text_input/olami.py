80096a3f8afe57d95b71c3c33b4d1a8e8a88bd66
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-12 10:43:22 +0800

    olami

--- text_input/olami.py
+++ text_input/olami.py
@@ -5,7 +5,7 @@ from cht_package.config import OLAMI_APP_KEY, OLAMI_APP_SECRET, OLAMI_URL
 def OLAMI_textInput(input):
     nluApi = NluAPISample()
     nluApi.setLocalization(OLAMI_URL)
-    nluApi.setAuthorization(OLAMI_APP_KEY, OLAMI_APP_SECRET)     
+    nluApi.setAuthorization('c97b4c08105144ee874c7329aa9e076a', '17256f7d09e34f8983cd20f60c5d9ae8')     
     
     print("\n---------- Test NLU API, api=seg ----------\n");
     print("\nResult:\n\n", nluApi.getRecognitionResult(nluApi.API_NAME_SEG, input))
@@ -13,4 +13,4 @@ def OLAMI_textInput(input):
     print("\n---------- Test NLU API, api=nli ----------\n");
     print("\nResult:\n\n", nluApi.getRecognitionResult(nluApi.API_NAME_NLI, input))
 
-    #return nluApi.getRecognitionResult(nluApi.API_NAME_NLI, input)
+    return nluApi.getRecognitionResult(nluApi.API_NAME_NLI, input)
