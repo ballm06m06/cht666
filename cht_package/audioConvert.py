f70ff113a66fb6b77ff430d0012fc4e76df5266d
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-12 21:52:13 +0800

    towav

--- cht_package/audioConvert.py
+++ cht_package/audioConvert.py
@@ -6,6 +6,6 @@ def toWAV(old_path, new_path):
     sound = AudioSegment.from_file(old_path, "m4a")
     sound.export(new_path, format="wav")
     os.remove(old_path)
-    print('old remove ')
+    print('old remove ok')
 
     return new_path
