0ae521617a8643cf01fa1359e223357d361d2055
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-12 21:10:20 +0800

    postgresql

--- cht_package/audioConvert.py
+++ cht_package/audioConvert.py
@@ -2,6 +2,7 @@ from pydub import AudioSegment
 import os, tempfile
 
 def toWAV(old_path, new_path):
+    print('old:'+old_path+'\nnew: '+new_path)
     sound = AudioSegment.from_file(old_path, "m4a")
     sound.export(new_path+'.wav', format="wav")
     os.remove(old_path)
