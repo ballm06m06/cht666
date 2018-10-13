72fb788ed2e7e2bd6b2efe781d61b0f0bf818f7b
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-13 11:27:47 +0800

    os walk

--- cht_package/audioConvert.py
+++ cht_package/audioConvert.py
@@ -6,13 +6,13 @@ static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
 
 def toWAV(old_path, new_path):
-    print(old_path+ ' '+ new_path)
+    #print(old_path+ ' '+ new_path)
     sound = AudioSegment.from_file(old_path, "m4a")
     sound.export(new_path, format="wav")
     os.remove(old_path)
     print('old audio file remove ok')
 
-    for root, dirs, files in walk(static_tmp_path):
+    for root, dirs, files in walk('/static/tmp'):
         print("路徑：", root)
         print("  目錄：", dirs)
         print("  檔案：", files)
