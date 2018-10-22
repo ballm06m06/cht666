c3844a4e51325a9fc72465aa6d75676788987810
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-22 11:43:30 +0800

    intro

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def handle_message(event):
     if isinstance(event.message, TextMessage):
         msg = event.message.text #message from user
 
-        if global first_add:
+        if first_add == True:
             line_single_push(profile.user_id, '新來的喔')
             first_add = False
             return 0
