79363e589a6c50de9343a7939dacbf8a0a599e5c
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-22 11:42:03 +0800

    intro

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def handle_message(event):
         if global first_add:
             line_single_push(profile.user_id, '新來的喔')
             first_add = False
-
+            return 0
         #quick reply test
         if msg == '123':
             line_bot_api.reply_message(
