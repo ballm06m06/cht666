c0b02ee7e0ead62c74fa2cae4ace38e6947764d1
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-28 04:03:14 +0800

    test

--- app.py
+++ app.py
@@ -373,6 +373,30 @@ def get_userIntent(id, name, msg):
     elif intent == 'help':
         line_single_push(id, 'help')
     
+    elif intent == '氣象':
+        
+        try:
+            #OLAMI get weather info
+            olamiJson = json.loads(OLAMI_textInput(msg))
+            #best
+            response = olamiJson["data"]["nli"][0]["desc_obj"]["result"]
+
+            temperature_low = olamiJson["data"]["nli"][0]["data_obj"][0]["temperature_low"]
+            temperature_high = olamiJson["data"]["nli"][0]["data_obj"][0]["temperature_high"]
+            #桃園市中壢區
+            city = olamiJson["data"]["nli"][0]["data_obj"][0]['city']
+            #['2018年10月25日', '晴', '東北東風和風', '最高溫度25.0℃', '最低溫度21.8℃。']
+            description = olamiJson["data"]["nli"][0]["data_obj"][0]['description'].split(',')
+
+
+            #line_single_push(id, description[0]+description[1])
+            line_single_push(id, response)
+
+        except Exception as e:
+            line_single_push(id, '對不起，您的說法我還不懂，能換個說法嗎？')
+            print('get weather info exception:'+str(e))
+
+    # intent: none >> OLAMI(天氣、閒聊...)
     else:
         try:
             #OLAMI TEXT
