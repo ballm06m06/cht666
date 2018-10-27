e26ed3b843c3e25cf33d9610653c84ef8de88203
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-28 04:29:10 +0800

    test

--- bot/template.py
+++ bot/template.py
@@ -178,7 +178,7 @@ def get_totalFishStatus(count, mlist, ph, do, tmp , id):
     fatal_url="https://i.imgur.com/eVUPJvP.png"
 
     i = datetime.datetime.now()
-    mdatetime = '%s-%s-%s' % (i.year, i.month, i.day) +'  '+ str(i.hour-4)+':'+str(i.minute)
+    mdatetime = '%s-%s-%s' % (i.year, i.month, i.day) +'  '+ str(i.hour)+':'+str(i.minute)
     print(i.strftime('%H:%M'))
     print('%s-%s-%s' % (i.year, i.month, i.day))
 
