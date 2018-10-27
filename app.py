0c67da5c5237e61b5567b1d7d8ecff2bd94b7771
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-27 16:24:27 +0800

    testp

--- app.py
+++ app.py
@@ -142,13 +142,13 @@ def handle_message(event):
                                     TextComponent(text='魚塭資訊回傳', weight='bold', size='xxl', margin='md'),
                                     TextComponent(text='新北市板橋區', size='xs', color='#aaaaaa', margin='sm', wrap=True),
                                     SeparatorComponent(margin='xxl'),
-                                    TextComponent(text='種類', size="md", weight="bold", wrap=True),
+                                    TextComponent(text='種類', size="md", weight="bold", wrap=True, spacing='sm'),
                                 ]
                             ),
                             # fish
                             BoxComponent(
                                 layout='horizontal',
-                                margin='xs',
+                                margin='md',
                                 spacing='sm',
                                 contents=[
                                     
@@ -157,8 +157,46 @@ def handle_message(event):
                                     ImageComponent(size= "xs", aspectRatio="20:13", aspectMode="fit", url="https://i.imgur.com/6C044b5.png", align="end", gravity="center") 
                                 ]
                             ),
+
                             SeparatorComponent(margin='xxl'),
-                            TextComponent(text="水質資訊", size="md", weight="bold", wrap=True)
+                            TextComponent(text="水質資訊", size="md", weight="bold", wrap=True, spacing='sm')
+
+                            # water-ph
+                            BoxComponent(
+                                layout='horizontal',
+                                margin='md',
+                                spacing='sm',
+                                contents=[
+                                    
+                                    TextComponent(text="ph值", size="sm", color="#555555", flex=0),
+                                    
+                                    TextComponent(text="8.9", siz="sm", color="#111111", align="end")
+                                ]
+                            ),
+                            # water-do
+                            BoxComponent(
+                                layout='horizontal',
+                                margin='md',
+                                spacing='sm',
+                                contents=[
+                                    
+                                    TextComponent(text="溶氧量(mg/L)", size="sm", color="#555555", flex=0),
+                                    
+                                    TextComponent(text="3.5, siz="sm", color="#111111", align="end")
+                                ]
+                            ),
+                            # water-tmp
+                            BoxComponent(
+                                layout='horizontal',
+                                margin='md',
+                                spacing='sm',
+                                contents=[
+                                    
+                                    TextComponent(text="水溫(°C)", size="sm", color="#555555", flex=0),
+                                    
+                                    TextComponent(text="23", siz="sm", color="#111111", align="end")
+                                ]
+                            )
                         ]
                     ),
                     #
