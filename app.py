01a6205eae29863bfa4961624598a08e603cab94
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-27 16:17:40 +0800

    testp

--- app.py
+++ app.py
@@ -141,7 +141,8 @@ def handle_message(event):
                                     TextComponent(text='魚塭狀態', weight='bold', size='sm', color='#1DB446'),
                                     TextComponent(text='魚塭資訊回傳', weight='bold', size='xxl', margin='md'),
                                     TextComponent(text='新北市板橋區', size='xs', color='#aaaaaa', margin='sm', wrap=True),
-                                    SeparatorComponent(margin='xxl')
+                                    SeparatorComponent(margin='xxl'),
+                                    TextComponent(text='種類', size="md", weight="bold", wrap=True),
                                 ]
                             ),
                             # fish
@@ -150,18 +151,20 @@ def handle_message(event):
                                 margin='xs',
                                 spacing='sm',
                                 contents=[
-                                    TextComponent(text='種類', size="md", weight="bold", wrap=True),
+                                    
                                     TextComponent(text="吳郭魚", size="xl", wrap=True, gravity="center"),
                                     SeparatorComponent(gravity="center"),
                                     ImageComponent(size= "xs", aspectRatio="20:13", aspectMode="fit", url="https://i.imgur.com/6C044b5.png", align="end", gravity="center") 
                                 ]
                             ),
-                            SeparatorComponent(margin='xxl')
+                            SeparatorComponent(margin='xxl'),
+                            TextComponent(text="水質資訊", size="md", weight="bold", wrap=True)
                         ]
                     ),
+                    #
                     footer=BoxComponent(
                         layout='vertical',
-                        spacing='sm',
+                        spacing='md',
                         contents=[
                             TextComponent(text="日期時間", size="xs", color="#aaaaaa", flex=0),
                             TextComponent(text="2018-10-28 14:20", size="xs", color="#aaaaaa", align="end")
