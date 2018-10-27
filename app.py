7888cc855591f418911f56729543668ca80b4dd4
MingSheng Lyu <sam850118sam@gmail.com>
2018-10-27 14:54:18 +0800

    test

--- app.py
+++ app.py
@@ -129,112 +129,112 @@ def handle_message(event):
 
         #flex test
         elif msg == 'flex':
-            
-            bubble = BubbleContainer(
-            direction='ltr',
-            hero=ImageComponent(
-                url='https://example.com/cafe.jpg',
-                size='full',
-                aspect_ratio='20:13',
-                aspect_mode='cover',
-                action=URIAction(uri='http://example.com', label='label')
-            ),
-            body=BoxComponent(
-                layout='vertical',
-                contents=[
-                    # title
-                    TextComponent(text='魚塭狀態', weight='bold', size='sm', color='#1DB446'),
-                    # review
-                    BoxComponent(
-                        layout='baseline',
-                        margin='md',
-                        contents=[
-                            IconComponent(size='sm', url='https://i.imgur.com/QiTvzLn.png'),
-                            IconComponent(size='sm', url='https://example.com/grey_star.png'),
-                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                            IconComponent(size='sm', url='https://example.com/grey_star.png'),
-                            TextComponent(text='4.0', size='sm', color='#999999', margin='md',
-                                          flex=0)
-                        ]
+                bubble = BubbleContainer(
+                    direction='ltr',
+                    hero=ImageComponent(
+                        url='https://example.com/cafe.jpg',
+                        size='full',
+                        aspect_ratio='20:13',
+                        aspect_mode='cover',
+                        action=URIAction(uri='http://example.com', label='label')
                     ),
-                    # info
-                    BoxComponent(
+                    body=BoxComponent(
                         layout='vertical',
-                        margin='lg',
+                        contents=[
+                            # title
+                            TextComponent(text='魚塭狀態', weight='bold', size='xl'),
+                            # review
+                            BoxComponent(
+                                layout='baseline',
+                                margin='md',
+                                contents=[
+                                    IconComponent(size='sm', url='https://example.com/gold_star.png'),
+                                    IconComponent(size='sm', url='https://example.com/grey_star.png'),
+                                    IconComponent(size='sm', url='https://example.com/gold_star.png'),
+                                    IconComponent(size='sm', url='https://example.com/gold_star.png'),
+                                    IconComponent(size='sm', url='https://example.com/grey_star.png'),
+                                    TextComponent(text='4.0', size='sm', color='#999999', margin='md',
+                                                flex=0)
+                                ]
+                            ),
+                            # info
+                            BoxComponent(
+                                layout='vertical',
+                                margin='lg',
+                                spacing='sm',
+                                contents=[
+                                    BoxComponent(
+                                        layout='baseline',
+                                        spacing='sm',
+                                        contents=[
+                                            TextComponent(
+                                                text='Place',
+                                                color='#aaaaaa',
+                                                size='sm',
+                                                flex=1
+                                            ),
+                                            TextComponent(
+                                                text='Shinjuku, Tokyo',
+                                                wrap=True,
+                                                color='#666666',
+                                                size='sm',
+                                                flex=5
+                                            )
+                                        ],
+                                    ),
+                                    BoxComponent(
+                                        layout='baseline',
+                                        spacing='sm',
+                                        contents=[
+                                            TextComponent(
+                                                text='Time',
+                                                color='#aaaaaa',
+                                                size='sm',
+                                                flex=1
+                                            ),
+                                            TextComponent(
+                                                text="10:00 - 23:00",
+                                                wrap=True,
+                                                color='#666666',
+                                                size='sm',
+                                                flex=5,
+                                            ),
+                                        ],
+                                    ),
+                                ],
+                            )
+                        ],
+                    ),
+                    footer=BoxComponent(
+                        layout='vertical',
                         spacing='sm',
                         contents=[
-                            BoxComponent(
-                                layout='baseline',
-                                spacing='sm',
-                                contents=[
-                                    TextComponent(
-                                        text='Place',
-                                        color='#aaaaaa',
-                                        size='sm',
-                                        flex=1
-                                    ),
-                                    TextComponent(
-                                        text='Shinjuku, Tokyo',
-                                        wrap=True,
-                                        color='#666666',
-                                        size='sm',
-                                        flex=5
-                                    )
-                                ],
+                            # callAction, separator, websiteAction
+                            SpacerComponent(size='sm'),
+                            # callAction
+                            ButtonComponent(
+                                style='link',
+                                height='sm',
+                                action=URIAction(label='CALL', uri='tel:000000'),
                             ),
-                            BoxComponent(
-                                layout='baseline',
-                                spacing='sm',
-                                contents=[
-                                    TextComponent(
-                                        text='Time',
-                                        color='#aaaaaa',
-                                        size='sm',
-                                        flex=1
-                                    ),
-                                    TextComponent(
-                                        text="10:00 - 23:00",
-                                        wrap=True,
-                                        color='#666666',
-                                        size='sm',
-                                        flex=5,
-                                    ),
-                                ],
-                            ),
-                        ],
-                    )
-                ],
-            ),
-            footer=BoxComponent(
-                layout='vertical',
-                spacing='sm',
-                contents=[
-                    # callAction, separator, websiteAction
-                    SpacerComponent(size='sm'),
-                    # callAction
-                    ButtonComponent(
-                        style='link',
-                        height='sm',
-                        action=URIAction(label='CALL', uri='tel:000000'),
+                            # separator
+                            SeparatorComponent(),
+                            # websiteAction
+                            ButtonComponent(
+                                style='link',
+                                height='sm',
+                                action=URIAction(label='WEBSITE', uri="https://example.com")
+                            )
+                        ]
                     ),
-                    # separator
-                    SeparatorComponent(),
-                    # websiteAction
-                    ButtonComponent(
-                        style='link',
-                        height='sm',
-                        action=URIAction(label='WEBSITE', uri="https://example.com")
-                    )
-                ]
-            ),
-            )
-            message = FlexSendMessage(alt_text="hello", contents=bubble)
-            line_bot_api.reply_message(
-                event.reply_token,
-                message
-            )
-            return 0
+                )
+                message = FlexSendMessage(alt_text="hello", contents=bubble)
+
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    message
+                )
+                return 0
 
         elif msg == 'btntem':
             
