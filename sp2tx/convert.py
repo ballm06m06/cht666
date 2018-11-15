dff6f4d0d467e5030731408a45bcd3933881972c
MingSheng Lyu <sam850118sam@gmail.com>
2018-11-15 19:04:13 +0800

    final

--- sp2tx/convert.py
+++ sp2tx/convert.py
@@ -7,7 +7,7 @@ from google.cloud.speech import enums
 from google.cloud.speech import types
 
 
-credential_path = "/app/sp2tx/credential/fintechbd99846f49cd.json"
+credential_path = "/app/sp2tx/credential/fintech-bd99846f49cd.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = crendential_path
 # Instantiates a client
 # client = speech.SpeechClient()
